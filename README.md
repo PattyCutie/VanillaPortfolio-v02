bd4951e5cbaabf0afbe69307acaec631557ccfb5
PattyCutie <iamsureiamcute@gmail.com>
2023-06-30 11:42:58 +0200

    clean file's structure

---
+++ README.md
@@ -0,0 +1,1 @@
+####Portfolio with html css js project learn to build the beautiful layout and data feching from local json data####
--- js/fetchdata.js
+++ js/fetchdata.js
@@ -1,13 +1,14 @@
 //Hero Section
-fetch("herodata.json")
+fetch("data/herodata.json")
   .then((response) => response.json())
   .then((data) => {
     // Access the herodata object
     const herodata = data.herodata;
 
     // Populate the HTML elements with the data
-    document.getElementById("fullName").textContent = herodata.fullName;
-    document.getElementById("herodescription").textContent = herodata.description;
+    document.getElementById("heroFullName").textContent = herodata.fullName;
+    document.getElementById("heroJobPosition").textContent =
+      herodata.jobPosition;
   })
   .catch((error) => {
     console.log("Error fetching JSON data:", error);
