0e704d99a4a286d272fe32589cc85a50bfe5b92e
PattyCutie <iamsureiamcute@gmail.com>
2023-08-02 19:16:10 +0200

    fix code, add project section, learn css Responsive

--- js/fetchdata.js
+++ js/fetchdata.js
@@ -3,19 +3,19 @@ fetch("data/herodata.json")
   .then((response) => response.json())
   .then((data) => {
     // Access the herodata object
-    const herodata = data.herodata;
+    const herodata = data.herodata
 
     // Populate the HTML elements with the data
-    document.getElementById("heroFullName").textContent = herodata.fullName;
+    document.getElementById("heroFullName").textContent = herodata.fullName
     document.getElementById("heroJobPosition").textContent =
-      herodata.jobPosition;
+      herodata.jobPosition
     document.getElementById("heroDescription").textContent =
-      herodata.description;
-    document.getElementById("heroEmail").textContent = herodata.email;
-    document.getElementById("heroWebsite").textContent = herodata.website;
+      herodata.description
+    document.getElementById("heroEmail").textContent = herodata.email
+    document.getElementById("heroWebsite").textContent = herodata.website
   })
   .catch((error) => {
-    console.log("Error fetching JSON data:", error);
-  });
+    console.log("Error fetching JSON data:", error)
+  })
 
 //About Section
